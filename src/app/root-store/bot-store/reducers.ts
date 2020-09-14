3662eba2ed0492370dbd5ee0a3b1ec41fa1f5d97
Not Ultiko <ljclaeyssen@gmail.com>
2020-09-14 18:42:12 +0200

    electron

--- src/app/root-store/bot-store/reducers.ts
+++ src/app/root-store/bot-store/reducers.ts
@@ -2,11 +2,15 @@ import {createReducer, on} from "@ngrx/store";
 import {initialState} from "./state";
 import * as actions from './actions';
 
-export const BotReducer = createReducer(
+export function BotReducer (state, action) {
+  return _BotReducer(state, action);
+}
+
+const _BotReducer = createReducer(
   initialState,
   on(actions.newConfiguration, (state, action) => ({
     ...state,
     configuration: action.configuration,
     activityLogs: [],
   })),
-)
+);
