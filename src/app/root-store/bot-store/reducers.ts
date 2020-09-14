b8c48e3b0ed27da55af23df9a972a799f54d9d94
Not Ultiko <ljclaeyssen@gmail.com>
2020-09-14 19:04:45 +0200

    clean reducer

--- src/app/root-store/bot-store/reducers.ts
+++ src/app/root-store/bot-store/reducers.ts
@@ -2,11 +2,7 @@ import {createReducer, on} from "@ngrx/store";
 import {initialState} from "./state";
 import * as actions from './actions';
 
-export function BotReducer (state, action) {
-  return _BotReducer(state, action);
-}
-
-const _BotReducer = createReducer(
+export const BotReducer = createReducer(
   initialState,
   on(actions.newConfiguration, (state, action) => ({
     ...state,
