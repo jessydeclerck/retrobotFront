14b016ffcd4d26457b1f8c84fc8a257401b2163e
Not Ultiko <ljclaeyssen@gmail.com>
2020-09-19 00:24:59 +0200

    need zoom/scroll dans une canvas

--- src/app/modules/map/map-container/map-container.component.ts
+++ src/app/modules/map/map-container/map-container.component.ts
@@ -6,39 +6,64 @@ import { mapfiles } from '../../../shared/constants/mapfiles'
   templateUrl: './map-container.component.html',
   styleUrls: ['./map-container.component.scss']
 })
-export class MapContainerComponent implements OnInit, AfterViewInit {
+export class MapContainerComponent implements AfterViewInit {
 
   ctx: CanvasRenderingContext2D;
   images: any[] = [];
+
+  public mapConfig = {
+    height: 13,
+    width:10,
+    mapShardHeight: 345,
+    mapShardWidth: 600,
+    cellsByRowOrCol: 15, //Because ce n'est pas un carré mais les cellules non plus are not des carrés
+    scale: 1
+  }
+
+  public cellConfig = {
+    cellHeight: this.mapConfig.mapShardHeight / this.mapConfig.cellsByRowOrCol * this.mapConfig.scale,
+    cellWidth: this.mapConfig.mapShardWidth / this.mapConfig.cellsByRowOrCol * this.mapConfig.scale,
+  }
   @ViewChild('mapCanvas') mapRef: ElementRef<HTMLCanvasElement>;
 
   constructor() { }
 
-  ngOnInit(): void {
 
+  private drawMapAndGrid(): void {
+    this.drawMap();
   }
 
-  paint(): void {
-
+  private drawMap(): void {
     mapfiles.forEach( (map, index) => {
       const coords = map.split('.png')[0].split('-');
       const newMap = new Image();
       newMap.onload = () => {
-        this.ctx.fillRect(0, 0, 15, 10);
-        const x = newMap.width * parseInt(coords[0]);
-        const y = newMap.height * parseInt(coords[1]);
-        console.log(x, y);
-        this.ctx.drawImage(newMap, x, y);
-        console.log(newMap)
+        const x = newMap.width * parseInt(coords[0]) * this.mapConfig.scale;
+        const y = newMap.height * parseInt(coords[1]) * this.mapConfig.scale;
+        this.ctx.drawImage(newMap, x, y, newMap.width* this.mapConfig.scale, newMap.height * this.mapConfig.scale);
+        this.drawCells(x, y)
       }
       newMap.src = '/assets/img/map/' + map;
       this.images.push(newMap);
     });
   }
 
+  private drawCells(x, y): void {
+    for (let i = 0 ; i < this.mapConfig.cellsByRowOrCol ; i++){
+      for (let j = 0 ; j < this.mapConfig.cellsByRowOrCol ; j ++) {
+        this.ctx.strokeRect(x + i * this.cellConfig.cellWidth, y + j * this.cellConfig.cellHeight, this.cellConfig.cellWidth, this.cellConfig.cellHeight)
+      }
+    }
+  }
+  public clic(event): void {
+    console.log(event);
+
+    this.ctx.fillRect(event.layerX, event.layerY, 5, 5);
+  }
+
   ngAfterViewInit(): void {
     this.ctx  = this.mapRef.nativeElement.getContext('2d');
-    this.paint();
+    this.drawMapAndGrid();
   }
 
 }
