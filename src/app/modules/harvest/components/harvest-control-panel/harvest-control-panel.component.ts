ec121a2f7eed11cb3f6f4ff09771d963f862ffb3
Jessy Declerck <declerck.jessy@gmail.com>
2020-09-23 15:26:06 +0200

    style: css gathering page

--- src/app/modules/harvest/components/harvest-control-panel/harvest-control-panel.component.ts
+++ src/app/modules/harvest/components/harvest-control-panel/harvest-control-panel.component.ts
@@ -1,15 +1,15 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {resourcesData} from "../../../../shared/constants/resourcesData";
 import {Store} from "@ngrx/store";
 import {RootStoreState} from "../../../../root-store";
 import {Observable} from "rxjs";
-import {BotStoreActions, BotStoreSelectors} from "../../../../root-store/bot-store";
 import {ScriptStoreActions, ScriptStoreSelectors} from "../../../../root-store/script-store";
 
 @Component({
   selector: 'app-harvest-control-panel',
   templateUrl: './harvest-control-panel.component.html',
-  styleUrls: ['./harvest-control-panel.component.scss']
+  styleUrls: ['./harvest-control-panel.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HarvestControlPanelComponent implements OnInit {
 
